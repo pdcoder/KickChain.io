dc4270ed144f6652692272c6fd237d9db435d2c6
pdcoder <pdas@inversion.co.in>
2019-01-19 17:32:01 +0530

    Added contracts

---
+++ contract/Inbox.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+contract Inbox {
+    string public message;
+
+    constructor (string memory initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
