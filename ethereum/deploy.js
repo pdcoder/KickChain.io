811bf38d6bf6c7122fb087b44e302a7125eba0f9
pdcoder <pdas@inversion.co.in>
2019-02-28 09:51:09 +0530

    Added deploy

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,4 +1,4 @@
-onst HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const compiledFactory = require('./build/CampaignFactory.json');
 
