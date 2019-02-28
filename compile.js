35a33093f35ec1a82f4fcf1068080e2db9d0a815
pdcoder <pdas@inversion.co.in>
2019-02-28 09:36:46 +0530

    Added deploy

--- compile.js
+++ compile.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const inboxpath = path.resolve(__dirname, 'contract','Inbox.sol');
+const inboxpath = path.resolve(__dirname, 'contract','inbox.sol');
 const source = fs.readFileSync(inboxpath,'utf8');
 
 module.exports = solc.compile(source,1).contracts[':Inbox'];
--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,8 +1,11 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-
+const {interface, bytecode} = require('../compile');
 const web3 = new Web3(ganache.provider());
+
+
+let inbox;
 
 beforeEach(()=>{
 
@@ -10,6 +13,9 @@ beforeEach(()=>{
     web3.eth.getAccounts().then( accounts => {
         console.log(accounts);
     });
+
+    new web3.eth.Contract(JSON.parse(interface)).deploy({data: bytecode, arguments: ['Hi there!']})
+    .send({ from : accounts[0], gas: '1,000,000'})
 });
 
 describe('Inbox',()=>{
